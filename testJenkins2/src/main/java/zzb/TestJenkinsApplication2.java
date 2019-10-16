e08f9fe301d2cf4ff762fab516b7c6bba43f3dff
zzb <p.moriarty@qq.com>
2019-10-16 11:32:09 +0800

    第二个工程

---
+++ testJenkins2/src/main/java/zzb/TestJenkinsApplication2.java
@@ -0,0 +1,14 @@
+package zzb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class TestJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsApplication.class, args);
+    }
+
+}
---
+++ testJenkins2/src/main/java/zzb/web/UserController.java
@@ -0,0 +1,15 @@
+package zzb.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/getUsers")
+    public Object getUsers() {
+        return "我是第二次修改成功!";
+    }
+
+}
